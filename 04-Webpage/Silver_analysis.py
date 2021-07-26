04e3eea5b81f8c9090a0a0901a136cb378a9aaec
Serrat96 <59369306+Serrat96@users.noreply.github.com>
2021-07-26 20:23:19 +0200

    try 5

--- 04-Webpage/Silver_analysis.py
+++ 04-Webpage/Silver_analysis.py
@@ -3,7 +3,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from datetime import *
-
+import os
+os.chdir('/app/centralbankanalytics/04-Webpage')
 # Ratio oro/plata
 gold_silver_ratio = pd.read_pickle(r'..\02-Data\03-Gold_vs_silver_ratio\01-Gold_vs_silver_ratio.pkl')
 gold_silver_ratio_graph = px.line(gold_silver_ratio, labels={
