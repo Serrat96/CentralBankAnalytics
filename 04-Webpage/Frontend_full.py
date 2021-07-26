10be286271607aa6f52667bdf26cc6d5de785ec8
Serrat96 <59369306+Serrat96@users.noreply.github.com>
2021-07-26 18:45:15 +0200

    deploy_try

--- 04-Webpage/Frontend_full.py
+++ 04-Webpage/Frontend_full.py
@@ -1,7 +1,8 @@
 import streamlit as st
-#import os
+import os
+print(os.getcwd())
 #os.chdir(r'C:\Users\serra\REPOSITORIOS\CentralBankAnalytics\04-Webpage')
-import Silver_analysis as sa
+'''import Silver_analysis as sa
 
 st.set_page_config(page_title='Central Bank Analytics', page_icon='euro')
 
@@ -56,3 +57,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+'''
