14f9562e8b488eeee2ecb7e818fdfce98f85fc31
Serrat96 <59369306+Serrat96@users.noreply.github.com>
2021-07-28 17:55:21 +0200

    fff

--- 04-Webpage/functions.py
+++ 04-Webpage/functions.py
@@ -4,7 +4,7 @@ import pathlib
 
 def read_data(path: str):
         try:
-                pd.read_parquet(pathlib.Path(path).absolute())
+                return pd.read_parquet(pathlib.Path(path).absolute())
         except:
                 path_2 = path[3:]
                 return pd.read_parquet(pathlib.Path(path).parent.absolute())
