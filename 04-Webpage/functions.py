387c718fcc2efeddd09e1bca7473931db715f82b
Serrat96 <59369306+Serrat96@users.noreply.github.com>
2021-07-28 21:01:55 +0200

    enesima

--- 04-Webpage/functions.py
+++ 04-Webpage/functions.py
@@ -1,11 +1,11 @@
 import pandas as pd
-import pathlib
 
 
 def read_data(path: str):
+        initial_path = r'/app/centralbankanalytics/'
         try:
-                return pd.read_parquet(pathlib.Path(path).absolute())
+                return pd.read_parquet(path)
         except:
                 path_2 = path[3:]
-                return pd.read_parquet(pathlib.Path(path).parent.absolute())
+                return pd.read_parquet(initial_path + path_2)
 
--- 04-Webpage/pruebas.py
+++ 04-Webpage/pruebas.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import pathlib
-
+initial_path = r'/app/centralbankanalytics/'
 path = r'..\02-Data\03-Gold_vs_silver_ratio\01-Gold_vs_silver_ratio.parquet'
 a = pd.read_parquet(pathlib.Path(path).absolute())
 
 path_2 = path[3:]
-cwd = pathlib.Path(__file__).resolve().parent
+cwd = initial_path + path_2
 print(cwd)
